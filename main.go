49f6bda0126ba5a8eff707d14e3a47650d66ef4a
kushagratwentyfour <58117595+kushagratwentyfour@users.noreply.github.com>
2022-04-28 10:12:16 +0530

    branch

--- main.go
+++ main.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
-	"sync"
-	"time"
 )
 
-var wg sync.WaitGroup
+//var wg sync.WaitGroup
 
 // const time = time.ParseDuration("1s").Milliseconds()
 
 func main() {
-	var delay = time.Duration(1000) * time.Millisecond
+	//	var delay = time.Duration(1000) * time.Millisecond
 
 	// loop the function until terminated
 	// for {
@@ -31,12 +30,12 @@ func main() {
 	// 	// 	// we repeat the function
 	// 	// }
 	// }
-	for {
-		wg.Add(1)
-		go log_analysis()
-		wg.Wait()
-		time.After(delay)
-	}
+	//for {
+	//wg.Add(1)
+	log_analysis()
+	//wg.Wait()
+	//	time.After(delay)
+	//}
 }
 func log_analysis() {
 	if _, err := os.Stat("./photon.log"); err != nil {
@@ -46,7 +45,6 @@ func log_analysis() {
 	f, err := os.Open("./photon.log")
 	if err != nil {
 		log.Printf("File can't be open %v", err)
-
 	}
 	// fmt.Println("file data", file)
 	defer func() {
@@ -54,23 +52,43 @@ func log_analysis() {
 			log.Fatal(err)
 		}
 	}()
+
 	s := bufio.NewScanner(f)
 	i := 0
 	// count := 0
 	var freq = make(map[string]int)
 	// var txns []string
+
 	for s.Scan() {
-		a := strings.Split(s.Text(), " ")
-		if a[8] == "block" {
-			txns := strings.Split(a[len(a)-3], "=")
-			total_txns, _ := strconv.Atoi(txns[1])
-			if _, ok := freq[a[2]]; ok {
-				freq[a[2]] = freq[a[2]] + total_txns
-			} else {
-				freq[a[2]] = total_txns
-			}
+
+		matched, err := regexp.Match("block", []byte(s.Text()))
+		if err != nil {
+			fmt.Print("\n found", err)
+
 		}
 
+		//fmt.Println(matched)
+
+		if matched == true {
+
+			//txns := strings.Split(a[len(a)-3], "=")
+			a := strings.Split(s.Text(), " ")
+
+			for _, j := range a {
+
+				if strings.HasPrefix(j, "txs=") {
+					total_txns, _ := strconv.Atoi(strings.Split(j, "=")[1])
+					if _, ok := freq[a[2]]; ok {
+						freq[a[2]] = freq[a[2]] + total_txns
+					} else {
+						freq[a[2]] = total_txns
+
+					}
+					//	fmt.Println(total_txns, a[2])
+				}
+
+			}
+		}
 		i++
 	}
 	max := 0
@@ -81,15 +99,15 @@ func log_analysis() {
 			max_txn = make(map[string]int)
 			max_txn[i] = j
 		}
-		// fmt.Println("New Block", i, j)
 	}
 	err = s.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for i, j := range max_txn {
 
-		fmt.Printf("Maximum transaction is %v at %v", j, i)
+		fmt.Printf("Maximum transaction is %v at %v\n", j, i)
 	}
-	wg.Done()
+	//wg.Done()
 }
